73b1fd6dcc5619319bc212b5f7d4ab44e9fc19c9
Bruno Nascimento <sunr00t@gmail.com>
2022-12-07 00:18:27 -0300

    update container services

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,20 @@
 import { Application } from "@hotwired/stimulus"
+// import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+// // Import and register all TailwindCSS Components
+// application.register('alert', Alert)
+// application.register('autosave', Autosave)
+// application.register('dropdown', Dropdown)
+// application.register('modal', Modal)
+// application.register('tabs', Tabs)
+// application.register('popover', Popover)
+// application.register('toggle', Toggle)
+// application.register('slideover', Slideover)
 
 export { application }
